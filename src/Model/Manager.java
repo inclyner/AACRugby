e80c527b4ca80fb53aa85107687024b581994970
inclyner <inclyner@gmail.com>
2022-11-26 12:11:42 +0000

    Added Model class and organized project files

---
+++ src/Model/Manager.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Manager {
+
+    public Manager() {
+    }
+
+    private void InsertUser(){}
+
+    private void deleteUser(){}
+
+    private void AproveChangeRequest(){}
+
+}
