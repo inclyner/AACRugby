61e62287175c50971edbc8709b6dd61f4bee4f24
Maria Carrilho <mariacarrilho.05@gmail.com>
2022-11-27 11:38:13 +0000

    login

---
+++ src/logic/Manager.java
@@ -0,0 +1,17 @@
+package logic;
+
+import java.sql.SQLException;
+
+public class Manager extends commonFeatures{
+
+    public Manager() throws SQLException {
+        super();
+    }
+
+    private void InsertUser(){}
+
+    private void deleteUser(){}
+
+    private void AproveChangeRequest(){}
+
+}
---
+++ src/logic/commonFeatures.java
@@ -0,0 +1,33 @@
+package logic;
+
+import java.sql.*;
+
+public abstract class commonFeatures {
+    private final String DATABASE_URL = "jdbc:sqlite:bd/AACRugby.db";
+    private Connection dbConn;
+
+    public commonFeatures() throws SQLException
+    {
+        dbConn = DriverManager.getConnection(DATABASE_URL);
+    }
+    //String sqlQuery = "SELECT nCC, logged, email, name, password, birthDate, sex, phoneNumber, aptitude, position, weight, height, typeUser FROM users";
+    public boolean login(String email, String password) throws SQLException {
+        Statement statement = dbConn.createStatement();
+
+        String sqlQuery = "SELECT email, password from users";
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+        while (resultSet.next()) {
+            String e = resultSet.getString("username");
+            String p = resultSet.getString("password");
+            if(email.equals(e) && password.equals(p)) {
+                resultSet.close();
+                statement.close();
+                return true;
+            }
+        }
+        resultSet.close();
+        statement.close();
+        return false;
+    }
+}
