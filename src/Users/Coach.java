37cda97c7d9c893627dfb91035df1f26604996cb
Maria Carrilho <mariacarrilho.05@gmail.com>
2022-11-27 23:50:24 +0000

    Changes by rodri

--- src/Users/Coach.java
+++ src/Users/Coach.java
@@ -1,6 +1,11 @@
 package Users;
 
-public class Coach {
+import java.sql.SQLException;
 
+public class Coach extends CommonFeatures{
+
+    public Coach(Long nCC, String name, String email, String sex, String birthDate, Long phoneNumber) throws SQLException {
+        super(nCC, name, email, sex, birthDate, phoneNumber);
+    }
 
 }
