df293c42c8ce30cff3ba7a42829beea80915e709
inclyner <inclyner@gmail.com>
2022-12-04 12:17:43 +0000

    Player by Joao

--- src/Users/Player.java
+++ src/Users/Player.java
@@ -1,6 +1,8 @@
 package Users;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Player extends CommonFeatures{
 
@@ -26,8 +28,21 @@ public class Player extends CommonFeatures{
         this.position = position;
     }
 
-    public void consultPersonalData(){}
 
-    public void requestChangePersonalData(String oldInfo, String newInfo) {}
+    public void requestChangePersonalData(String oldInfo, String newInfo) {
+        try {
+            Statement statement = getDbConnection().createStatement();
+            oldInfo = "SELECT email, phoneNumber FROM user WHERE type=2";
+            ResultSet resultSet = statement.executeQuery(oldInfo);
+            while(resultSet.next()) {
+                newInfo = "UPDATE requestChange SET email='" + email + "', " +
+                        "phoneNumber='" + phoneNumber + "' WHERE playerCC=" + nCC;;
+                statement.executeUpdate(newInfo);
+                statement.close();
+            }
+        }catch (SQLException e){
+            throw new RuntimeException();
+        }
+    }
 
 }
