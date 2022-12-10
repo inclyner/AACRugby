722d41298c9d4bf22aeb4ba3961851f12c268437
BrunoMartibns <brunodpmartins@gmail.com>
2022-12-10 10:55:07 +0000

    Acrescento da verificacao do type login

--- src/logic/ModelManager.java
+++ src/logic/ModelManager.java
@@ -33,5 +33,27 @@ public class ModelManager {
         statement.close();
         return false;
     }
+
+    public int checksTypeUser(String email, String password) throws SQLException{
+        File f = new File("bd\\AACRugby.db");
+        String DATABASE_URL = "jdbc:sqlite:" + f.getAbsolutePath();
+        Connection dbConn = DriverManager.getConnection(DATABASE_URL);
+        Statement statement = dbConn.createStatement();
+        String sqlQuery = "SELECT email, password, typeUserIc from user";
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+        while (resultSet.next()) {
+            String e = resultSet.getString("email");
+            String p = resultSet.getString("password");
+            if(email.equals(e) && password.equals(p)) {
+                int type = resultSet.getInt("typeUserId");
+                resultSet.close();
+                statement.close();
+                return type;
+            }
+        }
+        resultSet.close();
+        statement.close();
+        return 0;
+    }
 }
 
