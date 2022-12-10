ecab112f7c4a30cd4dc45f688d249f932300ebdf
Maria Carrilho <mariacarrilho.05@gmail.com>
2022-12-10 18:34:27 +0000

    login

--- src/logic/tests/ModelManagerTest.java
+++ src/logic/tests/ModelManagerTest.java
@@ -2,7 +2,7 @@ package logic.tests;
 
 import Users.Player;
 import logic.ModelManager;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
