857ce277cda4cd49f4e26f9f429c805d5f347d9c
inclyner <inclyner@gmail.com>
2022-12-12 00:09:15 +0000

    Send Email

--- src/logic/SendEmail.java
+++ src/logic/SendEmail.java
@@ -1,36 +1,59 @@
 package logic;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
+import com.aspose.email.MailAddress;
+import com.aspose.email.MailMessage;
+import com.aspose.email.SmtpClient;
+
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.nio.charset.Charset;
+import java.util.Date;
 import java.util.Properties;
 
 public class SendEmail {
     private String sendTo;
-    private final String sendFrom = "gps.aacrugby@gmail.com";
-    private final String host = "localhost";
+    private final String sendFrom = "gps.aacrugby@hotmail.com";
     public SendEmail(String sendTo){
         this.sendTo = sendTo;
     }
 
-    public boolean sendEmail(String password){
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
-        Session session = Session.getDefaultInstance(properties);
-        try{
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(sendFrom));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendTo));
-            message.setSubject("Account Validation");
-            message.setText("You have been added to the AACRugby Team App.\n Please login with this email and the password "+ password +".");
-            Transport.send(message);
-            return true;
-        }catch (MessagingException mex){
-            return false;
+    public boolean sendEmail(String password) throws MessagingException {
+        MailMessage message = new MailMessage();
+
+// Set subject of the message, body and sender information
+        message.setFrom(new MailAddress(sendFrom, "AACRugby", false));
+        message.setSubject("Account Validation");
+        message.setBody("You have been added to the AACRugby Team App.\n Please login with this email and the password "+ password +".");
+
+// Specify encoding
+        message.setBodyEncoding(Charset.forName("US-ASCII"));
+
+// Add To recipients and CC recipients
+        message.getTo().addItem(new MailAddress(sendTo, "Recipient 1", false));
+
+// Create an instance of SmtpClient Class
+        SmtpClient client = new SmtpClient();
+
+// Specify your mailing host server, Username, Password, Port
+        client.setHost("smtp-mail.outlook.com");
+        client.setUsername(sendFrom);
+        client.setPassword("aac.TEAM13");
+        client.setPort(587);
+
+        try
+        {
+            // Client.Send will send this message
+            client.send(message);
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
         }
+        return true;
+    }
+
+    public static void main(String[] args) throws MessagingException {
+        SendEmail sendEmail = new SendEmail("mariacarrilho.05@gmail.com");
+        sendEmail.sendEmail("allo");
     }
 }
